7c1a1dad437958af717e5f8f40dd9ec0109687c9
Malik B. Parker <mbparker@andrew.cmu.edu>
2019-08-13 16:28:07 -0400

    Added writing to a csv Feature

--- Project_Scrape.py
+++ Project_Scrape.py
@@ -6,6 +6,7 @@ from selenium.webdriver.remote.command import Command
 from os import system
 import string
 import re
+import csv
 
 
 def loadPageUntilID(id):
@@ -25,7 +26,7 @@ def appSetup():
 def Search(house_num, st_name, st_type):
     global browser
     # [Uncomment] This is a new feature
-    # browser.get("http://www2.county.allegheny.pa.us/RealEstate/Search.aspx")
+    browser.get("http://www2.county.allegheny.pa.us/RealEstate/Search.aspx")
 
     # Remove any puncations and Uppercase the letters
     st_name = st_name.translate(str.maketrans('', '', string.punctuation)).upper()
@@ -57,7 +58,7 @@ def Search(house_num, st_name, st_type):
     city, state, zipcode = addrParser(st_type, addr)
     address = house_num + " " + st_name + " " + st_type
 
-    return address, city, state, zipcode, owner
+    return [address, city, state, zipcode, owner]
 
 # Returns City, State, and ZipCode
 def addrParser(st_type, addr):
@@ -85,17 +86,27 @@ def main():
     # output = input("Name of output file: ")
 
     browser = webdriver.Safari()
+
+    ## Leads Input File
     # file = open(leads, 'r')
     # muni = file.read()
     # file.close()
 
-    appSetup()
-    addr, city, state, zipcode, name = Search("6419", "Deary", "St")
-    print(name)
-    print(addr)
-    print(city + " " + state + ", " + zipcode)
-    browser.quit()
+    ## Property Output File
+    properties = open('test.csv', mode='w')
+    prop_writer = csv.writer(properties, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    prop_writer.writerow(['Address', 'City', 'State', 'ZipCode', 'Name'])
 
+    appSetup()
+    property = Search("6419", "Deary", "St")
+    prop_writer.writerow(property)
+    property = Search("1840", "Jancey", "st")
+    prop_writer.writerow(property)
+
+
+    # Close all the files that are open
+    properties.close()
+    browser.quit()
 
 
 if __name__ == "__main__":
