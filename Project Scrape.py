f3dea305eee31a5530db656b4cf420ca0bc353ca
Darius <divitiaep@gmail.com>
2019-08-13 16:20:20 -0400

    Read and parse file

--- Project Scrape.py
+++ Project Scrape.py
@@ -2,42 +2,74 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import os
+import re
 
-browser = webdriver.Safari()
+# browser = webdriver.Safari()
+#
+# browser.get("http://www2.county.allegheny.pa.us/RealEstate/Default.aspx")
+#
+# # browser.maximize_window()
+#
+# Agree = browser.find_element_by_id("btnContinue")
+# Agree.click()
+#
+# try:
+#     element = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "txtStreetNum")))
+# except:
+#     pass
+# HouseNum = browser.find_element_by_id("txtStreetNum")
+# HouseNum.send_keys("1840")
+#
+# Street = browser.find_element_by_id("txtStreetName")
+# Street.send_keys("JANCEY")
+#
+# go = browser.find_element_by_id("btnSearch")
+# go.click()
+#
+# try:
+#     element = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "BasicInfo1_lblParcelID")))
+# except:
+#     pass
+#
+#
+# ParcelId = browser.find_element_by_id("BasicInfo1_lblParcelID")
+# # print("Parcel ID: " + ParcelId.text)
+#
+# Address = browser.find_element_by_id("BasicInfo1_lblAddress")
+# # print("Address: " + Address.text)
+#
+# owner = browser.find_element_by_id("BasicInfo1_lblOwner")
+# # print("Owner: " + owner.text)
+#
+# browser.quit()
 
-browser.get("http://www2.county.allegheny.pa.us/RealEstate/Default.aspx")
+FileName = os.path.expanduser("~/Documents/Properties.txt")
 
-# browser.maximize_window()
+PropFile = open(FileName,"r+")
 
-Agree = browser.find_element_by_id("btnContinue")
-Agree.click()
+with open(FileName) as f:
+    lines = [line.rstrip('\n') for line in open(FileName)]
 
-try:
-    element = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "txtStreetNum")))
-except:
-    pass
-HouseNum = browser.find_element_by_id("txtStreetNum")
-HouseNum.send_keys("1840")
+addresses = []
 
-Street = browser.find_element_by_id("txtStreetName")
-Street.send_keys("JANCEY")
+for items in lines:
+    lowercase = items.lower()
+    if "north" in lowercase:
+        newStr = lowercase.replace("north", "N")
+        addresses.append(newStr)
+    elif "south" in lowercase:
+        newStr = lowercase.replace("south", "S")
+        newStr = addresses.append(newStr)
+    elif "east" in lowercase:
+        newStr = lowercase.replace("east", "E")
+        newStr = addresses.append(newStr)
+    elif "west" in lowercase:
+        newStr = lowercase.replace("west", "W")
+        newStr = addresses.append(newStr)
+    else:
+        addresses.append(items)
 
-go = browser.find_element_by_id("btnSearch")
-go.click()
+    print(items)
+print(addresses)
 
-try:
-    element = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "BasicInfo1_lblParcelID")))
-except:
-    pass
-
-
-ParcelId = browser.find_element_by_id("BasicInfo1_lblParcelID")
-print("Parcel ID: " + ParcelId.text)
-
-Address = browser.find_element_by_id("BasicInfo1_lblAddress")
-print("Address" + Address.text)
-
-owner = browser.find_element_by_id("BasicInfo1_lblOwner")
-print("Owner: " + owner.text)
-
-browser.quit()
